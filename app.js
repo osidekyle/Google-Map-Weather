d233d6bea100b3126353d0ca1da252f9013f9ac0
osidekyle <hawkinskyle67@gmail.com>
2020-07-05 15:12:00 -0700

    got the map in

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
-$(window).on("load",function(){
+$(document).ready(function(){
+    $("#mainContainer").find("*").hide().fadeIn(750);
+    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCqN6r9BrxrrQV-MZlrgIJVNrng8M3X578&callback=initMap");
+});
 
-function
+var latitude;
+var longitude;
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(pos=>{
+        latitude=pos.coords.latitude;
+        longitude=pos.coords.longitude;
+    })
+}
 
+function initMap(){ 
+    var options={
+        zoom:4,
+        center:{lat:latitude,lng:longitude}
+    }
+    var map=new google.maps.Map(document.getElementById("googleMap"),options);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
