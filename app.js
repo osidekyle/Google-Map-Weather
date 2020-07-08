4ce00c12061e38cd82665f72a83878f6d921e9bb
osidekyle <hawkinskyle67@gmail.com>
2020-07-07 23:39:46 -0700

    made it so you get the lat and long when clicked

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
-$(document).ready(function(){
+$(window).on("load",function(){
     $("#mainContainer").find("*").hide().fadeIn(750);
-    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCqN6r9BrxrrQV-MZlrgIJVNrng8M3X578&callback=initMap");
-});
+    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCqN6r9BrxrrQV-MZlrgIJVNrng8M3X578&callback=initMap")
+    })
 
-var latitude;
-var longitude;
+
+
+
+
+
+var mylatitude;
+var mylongitude;
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(pos=>{
-        latitude=pos.coords.latitude;
-        longitude=pos.coords.longitude;
+        mylatitude=pos.coords.latitude;
+        mylongitude=pos.coords.longitude;
     })
 }
 
-function initMap(){ 
+function initMap(){
+    var latlng={lat:mylatitude,lng:mylongitude};
+    
+    var calcLat;
+    var calcLong;
+
     var options={
         zoom:4,
-        center:{lat:latitude,lng:longitude}
+        center:latlng,
     }
     var map=new google.maps.Map(document.getElementById("googleMap"),options);
+
+    var infoWindow=new google.maps.InfoWindow(
+        {content:"Click to show weather at that location",position:latlng});
+    infoWindow.open(map);
+
+
+    map.addListener("click",function(mapsMouseEvent){
+        calcLong=mapsMouseEvent.latLng.lng();
+        calcLat=mapsMouseEvent.latLng.lat();
+        console.log(`(${calcLat},${calcLong})`);
+    })
+
+
+
+
     }
+
+
+
+
+
+    
 
     
 
